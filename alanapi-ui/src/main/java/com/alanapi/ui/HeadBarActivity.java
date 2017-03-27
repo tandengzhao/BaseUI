1fef022d48dc366030621919825a5a0ee386a71a
OliverTan <alanapi@163.com>
2017-03-27 19:08:52 +0800

    优化 bug fix ui

--- alanapi-ui/src/main/java/com/alanapi/ui/HeadBarActivity.java
+++ alanapi-ui/src/main/java/com/alanapi/ui/HeadBarActivity.java
@@ -83,6 +83,11 @@ public abstract class HeadbarActivity extends Activity {
     showToolbarLeftBack();
     setContentView(getContentViewLayoutResID());
 
+    if(!getWindowTranslucentStatus()) {
+      rootActivityLayout.setFitsSystemWindows(false);
+      toolbar.setFitsSystemWindows(false);
+    }
+
     ViewUtil.setTextViewCompoundDrawableLeft(tvToolbarLeftBack, DrawableUtil.tintDrawable(this, DrawableUtil.getVectorDrawable(this, R.drawable.ui_back), R.color.ui_back));
 
     initView();
